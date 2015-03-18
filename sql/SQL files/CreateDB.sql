167989c7a79ac0341f8331db350fafaf946b7940
TeoGia <teogia89@gmail.com>
2015-03-18 21:56:44 +0200

    CRMServer-2 Initial Database Schema
    CreateDB.sql - Created more tables. Checks if table exists before each 'create table' statement.
    
    TestData.sql - Some test data.

--- sql/SQL files/CreateDB.sql
+++ sql/SQL files/CreateDB.sql
@@ -1,106 +1,182 @@
-CREATE TABLE Contacts (
-ID int IDENTITY(1,1) PRIMARY KEY,
-ReferenceNo int(9) IDENTITY(100000000,1) not null,
-LastName nvarchar(50) NOT NULL,
-FirstName nvarchar(50),
-Address nvarchar(255),
-StreetNo int,
-PostalCode int,
-City nvarchar(255),
-PrefectureID int FOREIGN KEY REFERENCES Prefectures(ID),
-CountryID int FOREIGN KEY REFERENCES Countries(ID),
-PhoneType1ID int FOREIGN KEY REFERENCES PhoneTypes(ID),
-PhoneType2ID int FOREIGN KEY REFERENCES PhoneTypes(ID),
-PhoneType3ID int FOREIGN KEY REFERENCES PhoneTypes(ID),
-PhoneType4ID int FOREIGN KEY REFERENCES PhoneTypes(ID),
-PhoneType5ID int FOREIGN KEY REFERENCES PhoneTypes(ID),
-Phone1 nvarchar(10),
-Phone2 nvarchar(10),
-Phone3 nvarchar(10),
-Phone4 nvarchar(10),
-Phone5 nvarchar(10),
-isPerson smallint,
-isAccount smallint,
-isCustomer smallint,
-isLead smallint,
-isSupplier smallint,
-isCompetitor smallint,
-isPartner smallint,
-isEmployee smallint,
-ProfessionID int FOREIGN KEY REFERENCES Professions(ID),
-TIN int,
-TaxOfficeID int FOREIGN KEY REFERENCES TaxOffices(ID),
-FathersName nvarchar(50),
-SpouceName carchar(50),
-MaritalStatus smallint,
-BirthDate datetime,
-isActive smallint,
-ActivationDate datetime not null default getdate(),
-DeactivationDate datetime,
-Sex smallint,
-Email1 nvarchar(100),
-Email2 nvarchar(100),
-WebSite nvarchar(255),
-Notes nvarchar(255),
-ContactImage image);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'Contacts' AND type = 'U')
+  BEGIN
+  CREATE TABLE Contacts (
+    ID int IDENTITY(1000000,1) PRIMARY KEY,
+    LastName nvarchar(50) NOT NULL,
+    FirstName nvarchar(50),
+    isPerson smallint,
+    isAccount smallint,
+    isCustomer smallint,
+    isLead smallint,
+    isSupplier smallint,
+    isCompetitor smallint,
+    isPartner smallint,
+    isEmployee smallint,
+    ProfessionID int FOREIGN KEY REFERENCES Professions(ID),
+    TIN int,
+    TaxOfficeID int FOREIGN KEY REFERENCES TaxOffices(ID),
+    MaritalStatus smallint,
+    BirthDate datetime,
+    isActive smallint,
+    ActivationDate datetime not null default getdate(),
+    DeactivationDate datetime,
+    Sex smallint,
+    Notes nvarchar(255),
+    ContactImage image);
+  END;
 
-create table Prefectures (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Description nvarchar(50) not null,
-CountryID int foreign key References Countries(ID) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'RelativeContacts' AND type = 'U')
+  BEGIN
+  create table RelativeContacts (
+    ID int identity (1,1) primary key,
+    ParentContactID int foreign key references Contacts(ID) not null,
+    ChildContactID int foreign key references Contacts(ID) not null);
+  END;
 
-create table Countries (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Description nvarchar(50) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ContactAddresses' AND type = 'U')
+  BEGIN
+  create table ContactAddresses (
+    ID int identity(1,1) primary KEY,
+    ContactID int foreign key references Contacts(ID) not null,
+    Adress nvarchar(255),
+    StreetNo int,
+    PostalCode int,
+    City nvarchar(255),
+    PrefectureID int FOREIGN KEY REFERENCES Prefectures(ID),
+    CountryID int FOREIGN KEY REFERENCES Countries(ID));
+  END;
 
-create table PhoneTypes (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Description nvarchar(20) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ContactPhones' AND type = 'U')
+  BEGIN
+  create table ContactPhones (
+    ID int identity(1,1) primary key,
+    ContactID int foreign key references Contacts(ID) not null,
+    PhoneTypeID int FOREIGN KEY REFERENCES PhoneTypes(ID),
+    Phone nvarchar(10));
+  END;
 
-create table Professions (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Description nvarchar(50) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ContactEmails' AND type = 'U')
+  BEGIN
+  create table ContactEmails (
+    ID int identity(1,1) primary key,
+    ContactID int foreign key references Contacts(ID) not null,
+    Email nvarchar(100));
+  END;
 
-create table TaxOffices (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Code int not null,
-Description nvarchar(50) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ContactWebSites' AND type = 'U')
+  BEGIN
+  create table ContactWebSites (
+    ID int identity(1,1) primary key,
+    ContactID int foreign key references Contacts(ID) not null,
+    WebSite nvarchar(255));
+  END;
 
-create table Users (
-ID int IDENTITY(1,1) Primary key,
-Username nvarchar(50) not null unique,
-Password nvarchar(255) not null unique,
-ContactID int Foreign key references Contacts(ID) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'Prefectures' AND type = 'U')
+  BEGIN
+  create table Prefectures (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Description nvarchar(50) not null,
+    CountryID int foreign key References Countries(ID) not null);
+  END;
 
-create table Activities (
-ID int IDENTITY(1,1) PRIMARY KEY,
-ReferenceNo int IDENTITY(311000000,1) not null,
-ActivityTypeID int Foreign key REFERENCES ActivityTypes(ID) not null,
-SubjectID int Foreign key References ActivitySubjects(ID) not null,
-Description nvarchar(100),
-Notes nvarchar(255),
-OwnerID int foreign key references Users(ID) not null,
-CreatorID int foreign key references Users(ID) not null,
-ContactID int Foreign Key References Contacts(ID) not null,
-StartDate datetime not null default getdate(),
-EndDate datetime);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'Countries' AND type = 'U')
+  BEGIN
+  create table Countries (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Description nvarchar(50) not null);
+  END;
 
-create table ActivityTypes (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Description nvarchar(50) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'PhoneTypes' AND type = 'U')
+  BEGIN
+  create table PhoneTypes (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Description nvarchar(20) not null);
+  END;
 
-create table ActivitySubjects (
-ID int IDENTITY(1,1) PRIMARY KEY,
-Description nvarchar(255) not null,
-ActivityTypeID int Foreign key references ActivityTypes(ID) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'Professions' AND type = 'U')
+  BEGIN
+  create table Professions (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Description nvarchar(50) not null);
+  END;
 
-create table ActivityHistory (
-ID int IDENTITY(1,1) PRIMARY KEY,
-ActivityID int foreign key references Activities(ID) not null;
-OwnerTimeStamp datetime not null default getdate(),
-OwnerID int foreign key references Activities(OwnerID) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'TaxOffices' AND type = 'U')
+  BEGIN
+  create table TaxOffices (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Code int not null,
+    Description nvarchar(50) not null);
+  END;
 
-create table ContactActivityHistory (
-ID int IDENTITY(1,1) PRIMARY KEY,
-ContactID int foreign key references Contacts(ID) not null,
-ActivityID int foreign key references Activities(ID) not null);
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'Users' AND type = 'U')
+  BEGIN
+  create table Users (
+    ID int IDENTITY(1,1) Primary key,
+    Username nvarchar(50) not null unique,
+    Password nvarchar(255) not null unique,
+    ContactID int Foreign key references Contacts(ID) not null);
+  END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'Activities' AND type = 'U')
+  BEGIN
+  create table Activities (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    ActivityTypeID int Foreign key REFERENCES ActivityTypes(ID) not null,
+    SubjectID int Foreign key References ActivitySubjects(ID) not null,
+    Description nvarchar(100),
+    Notes nvarchar(255),
+    OwnerID int foreign key references Users(ID) not null,
+    CreatorID int foreign key references Users(ID) not null,
+    ContactID int Foreign Key References Contacts(ID) not null,
+    StartDate datetime not null default getdate(),
+    EndDate datetime);
+  END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ActivityTypes' AND type = 'U')
+  BEGIN
+  create table ActivityTypes (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Description nvarchar(50) not null);
+  END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ActivitySubjects' AND type = 'U')
+  BEGIN
+  create table ActivitySubjects (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    Description nvarchar(255) not null,
+    ActivityTypeID int Foreign key references ActivityTypes(ID) not null);
+  END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ActivityHistory' AND type = 'U')
+  BEGIN
+  create table ActivityHistory (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    ActivityID int foreign key references Activities(ID) not null,
+    OwnerTimeStamp datetime not null default getdate(),
+    OwnerID int foreign key references Activities(OwnerID) not null);
+  END;
+
+IF NOT EXISTS (SELECT * FROM sys.tables
+WHERE name = N'ContactActivityHistory' AND type = 'U')
+  BEGIN
+  create table ContactActivityHistory (
+    ID int IDENTITY(1,1) PRIMARY KEY,
+    ContactID int foreign key references Contacts(ID) not null,
+    ActivityID int foreign key references Activities(ID) not null);
+  END;
--- sql/SQL files/TestData.sql
+++ sql/SQL files/TestData.sql
@@ -1,23 +1,33 @@
 insert into Countries (Description)
-values ('Greece'),('Elladistan'),('Kourdistan');
+  values ('Greece'),('Elladistan'),('Kourdistan');
 
 insert into Prefectures (Description, CountryID)
-values ('Attikh', 1), ('Argolida', 1), ('Achaia', 1);
+  values ('Attikh', 1), ('Argolida', 1), ('Achaia', 1);
 
 insert into PhoneTypes (Description)
-values ('Home'), ('Mobile'), ('Work'), ('Fax');
+  values ('Home'), ('Mobile'), ('Work'), ('Fax');
 
 insert into Professions (Description)
-values ('Freelancer'), ('Dimosio 4 ever'), ('Idiotikos malakas');
+  values ('Freelancer'), ('Dimosio 4 ever'), ('Idiotikos malakas');
 
 insert into TaxOffices (Code, Description)
-values (1010, 'Athinwn'), (1011, 'Axarnwn'), (1012, 'Naupliou'), (1013, 'Argous');
+  values (1010, 'Athinwn'), (1011, 'Axarnwn'), (1012, 'Naupliou'), (1013, 'Argous');
 
 insert into ActivityTypes (Description)
-values ('Call'), ('E-mail'), ('Fax'), ('Mail');
+  values ('Call'), ('E-mail'), ('Fax'), ('Mail');
 
 insert into ActivitySubjects (Description, ActivityTypeID)
-values ('Inbound Call', 1), ('Outbound Call', 1);
+  values ('Inbound Call', 1), ('Outbound Call', 1);
 
-insert into Contacts (Lastname, Firstname, Address, City, CountryID, PrefectureID, PhoneType1ID, Phone1, isPerson, isCustomer, ProfessionID, isActive)
-values ('Tarifas', 'Mitsos', 'Taksitzidwn 12', 'Piatsa', 1, 1, 1, '210905487234', 1, 1, 2, 1);
+insert into ContactAddresses (ContactID, Address, StreetNo, PostalCode, City, PrefectureID, CountryID)
+    values (1000000, 'Taksitzidwn', 12, 12345, 'Athina', 1, 1),
+      (1000001, 'Gatiwn', 2, 23456, 'Nafplio', 2, 1),
+      (1000002, 'Gatiwn', 3, 23456, 'Nafplio', 2, 1);
+
+insert into ContactPhones(ContactID, PhoneTypeID, Phone)
+    values (100000, 1, 2101234567);
+
+insert into Contacts (Lastname, Firstname, isPerson, isCustomer, ProfessionID, isActive)
+  values ('Tarifas', 'Mitsos', 1, 1, 2, 1),
+    ('Tsirkoulakis', 'Tsirkos', 1, 0, 1, 1),
+    ('Giakovakis', 'Giakovas', 1, 1, 1, 0);
